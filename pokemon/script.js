ae45eb8d836173795652ce00a9916ae52e1c8c29
Danny <contact@danfoster.io>
2017-12-20 14:46:09 -0600

    API

---
+++ pokemon/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    for(var i = 1; i <= 151; i++) {
+        $('.wrapper').append('<img src="http://pokeapi.co/media/sprites/pokemon/' + i + '.png"</img>');
+    }
+});
