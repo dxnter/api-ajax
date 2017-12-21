1331dfc8125d19fe9575ac4b245104311240bbb7
Danny <contact@danfoster.io>
2017-12-20 18:26:30 -0600

    Weather final

---
+++ weather-forecast/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('form').submit(function () {
+        var location = $('#locationInput').val();
+        console.log(location);
+        $.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&&appid=238984bd7c2291e393d7c9491ac01bd2`, function (res) {
+            var temp = Math.round((res.main.temp) * 9 / 5 - 459);
+            $('section').html(`<h1>${location}</h1><br><p>Temperature: ${temp}</p>`);
+        }, 'json');
+        return false;
+    });
+});
